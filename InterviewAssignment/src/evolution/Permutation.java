d4047550f68a37513b09bf2e4f96c9f95d595041
user <user@use1r-PC>
2017-02-14 17:16:48 +0800

    add some comments

--- InterviewAssignment/src/evolution/Permutation.java
+++ InterviewAssignment/src/evolution/Permutation.java
@@ -8,24 +8,24 @@ public class Permutation {
 		if (permutation == null) {
 			permutation = new int[m];
 		}
-		if (i < m) {
-			for (int j = 0; j < n; j++) {
-				if (!set.contains(j)) {
+		if (i < m) {// There are m layers.
+			for (int j = 0; j < n; j++) {// Each layer has n choices. 
+				if (!set.contains(j)) {// The choices should not be duplicate.
 					permutation[i] = j;
-					set.add(j);
+					set.add(j);// Add the choice in the set.
 					permutation(i + 1, m, n, permutation, set);
-					set.remove(j);
+					set.remove(j);// Remove the choice from the set.
 				}
 			}
 		} else {
 			for (int j = 0; j < m; j++) {
-				System.out.print(permutation[j]);
+				System.out.print(permutation[j]);// Print out the premutations.
 			}
 			System.out.println();
 		}
 	}
 	
 	public static void main(String[] args) {
-		permutation(0, 2, 7, null, new TreeSet<Integer>());
+		permutation(0, 3, 7, null, new TreeSet<Integer>());
 	}
 }
